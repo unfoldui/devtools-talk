3deee4e9043c5d58843f0eed24b6f6f0b58828d4
Ravi <rvisharma91@gmail.com>
2018-07-26 23:23:38 +0530

    Added console samples

--- public/client.js
+++
@@ -1,1 +0,0 @@
-console.info('Created by Ravi Sharma (@irvisharma)');
---
+++ public/debug.js
@@ -0,0 +1,11 @@
+function itsADeadCode(){
+    let first = 'Ravi';
+    let second = 'Sharma';
+    console.info('this function is useless.');
+}
+
+function callMe(){
+    let a = 1,
+        b = 2;
+    console.log(`I got called! and ${a} + ${b} = ${a+b}`);
+}
